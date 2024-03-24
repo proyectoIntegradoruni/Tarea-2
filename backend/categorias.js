6e0d2a5d8931a6d31248557b35c61de213cd1b13
Marcela <rivas.lina@correounivalle.edu.co>
2024-03-24 11:26:58 -0500

    creacion de categorias

---
+++ backend/categorias.js
@@ -0,0 +1,35 @@
+const saludos_y_presentaciones = [
+    "Hello", "Good morning", "Good afternoon", "Good evening", "How are you?",
+    "What's up?", "Nice to meet you", "Pleased to meet you", "Hi, what's your name?", "Where are you from?",
+    "What do you do?", "How old are you?", "Goodbye",  "See you later", "See you soon",
+    "Bye, have a good day", "Goodbye, take care", "How's it going?", "How was your day?", "Any news?",
+    "How's work?", "How was work?", "What are your plans for today?", "Shall we grab a coffee?", "Can we talk for a moment?",
+    "Can you help me, please?", "Excuse me", "I'm sorry", "Thank you", "Please"
+]
+
+const viajes_y_transportacion = [
+    "Airport", "Plane", "Ticket", "Bus", "Train",
+    "Taxi", "Hotel", "Check-in", "Check-out", "Reservation", "Suitcase",
+    "Luggage", "Terminal", "Boarding gate", "Departure", "Arrival",
+    "Passport", "Visa", "Immigration", "Customs", "Car rental",
+    "Train station", "Bus station", "Subway station", "Directions", "Map",
+    "Tourism", "Tour guide", "Attractions", "Excursion"
+]
+
+const comida_y_restaurantes = [
+    "Menu", "Menu card", "Starter", "Main course", "Dessert",
+    "Drink", "Water", "Wine", "Beer", "Coffee",
+    "Tea", "Breakfast", "Lunch", "Dinner", "Appetizer",
+    "Tapas", "Salad", "Soup", "Pasta",
+    "Rice", "Meat", "Fish", "Chicken", "Vegetables",
+    "Fruits", "Bread", "Cheese", "Dessert", "Ice cream"
+]
+
+const familia_y_mascotas = [
+    "Father", "Mother", "Brother", "Sister", "Nephew",
+    "Grandfather", "Grandmother", "Uncle", "Aunt", "Cousin",
+    "Niece", "Boy", "Girl", "Baby", "Husband",
+    "Wife", "Boyfriend", "Girlfriend", "Son", "Daughter",
+    "Pet", "Dog", "Cat", "Bird", "Fish",
+    "Rabbit", "Hamster", "Turtle"
+]
