1c70c2f52bdf9fafcae1624ef063554a683b974a
Marcela <rivas.lina@correounivalle.edu.co>
2024-03-24 11:03:14 -0500

    conexion con la IA

---
+++ backend/conection/ibm.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+const SpeechToTextV1 = require('ibm-watson/speech-to-text/v1');
+const { IamAuthenticator } = require('ibm-watson/auth');
+
+
+const speechToText = new SpeechToTextV1({
+  authenticator: new IamAuthenticator({
+    apikey: process.env.APIKEY,
+  }),
+  serviceUrl: process.env.URL,
+  disableSslVerification: true,
+});
+
+
+const params = {
+    objectMode: true,
+    contentType: 'audio/mp3',
+    model: 'es-CO_NarrowbandModel',
+    //keywords: ['colorado', 'tornado', 'tornadoes'],
+    //keywordsThreshold: 0.5,
+    //maxAlternatives: 3,
+  };
