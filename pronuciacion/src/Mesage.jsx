228b8ff04797ba1cec068cf3a3fc6f85bc5eb0e3
KPaola-M74 <moreno.karen@correounivalle.edu.co>
2024-03-26 15:41:25 -0500

    arreglos interfaz

--- pronuciacion/src/Mesage.jsx
+++ pronuciacion/src/Mesage.jsx
@@ -1,15 +1,16 @@
 import React, {useState } from "react";
 import { TypingIndicator } from '@chatscope/chat-ui-kit-react';
 import { useSpeechSynthesis } from 'react-speech-kit';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faVolumeUp } from '@fortawesome/free-solid-svg-icons'
 
 const Message = ({ content, timestamp, isOwner,reproduccion,text}) => {
     const ownerImageUrl = "https://img.freepik.com/vector-premium/icono-circulo-usuario-anonimo-ilustracion-vector-estilo-plano-sombra_520826-1931.jpg";
     const nonOwnerImageUrl = "https://cdn-icons-png.flaticon.com/512/3898/3898068.png";
-     const[palabra, setPalabra] =  useState("");
-     const { speak } = useSpeechSynthesis();
-     
+    const[palabra, setPalabra] =  useState("");
+    const { speak } = useSpeechSynthesis();
 
-    // Selecciona la URL de la imagen basada en el estado de isOwner
+
     const imageUrl = isOwner ? ownerImageUrl : nonOwnerImageUrl;
 
   
@@ -36,7 +37,12 @@ const Message = ({ content, timestamp, isOwner,reproduccion,text}) => {
       </div>
       {/* Mostrar el botón solo si reproduccion es true */}
       {reproduccion && (
-        <button onClick={reproducirPronunciacion}>Reproducir Pronunciación</button>
+        <FontAwesomeIcon 
+        icon={faVolumeUp} 
+        onClick={reproducirPronunciacion} 
+        style={{ cursor: 'pointer', fontSize: '2em' }} 
+      />
+       
       )}
     </div>
       
@@ -45,3 +51,4 @@ const Message = ({ content, timestamp, isOwner,reproduccion,text}) => {
 };
 
 export default Message;
+{ /*<button  className="login-form-btn2"  onClick={reproducirPronunciacion}>Reproducir Pronunciación</button>*/}
